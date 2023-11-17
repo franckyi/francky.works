be6b6531cc9920ede4e0d4f2398a3e69df3f0def
Francky <contact@francky.pl>
2023-11-17 01:12:48 +0100

    fix hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,14 +3,15 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="relative mt-12">
-      <div className="z-50 absolute bottom-2 -left-12 text-8xl font-bold text-emerald-400">
-        <span className="block text-amber-400">design</span>
-        <span className="">stronywww</span>
-        <span className="text-amber-400">aplikacjeweb</span>
+      <div className="z-50 absolute -bottom-2 -left-12 text-8xl font-bold text-emerald-400">
+        <div className="mx-auto shadow-stone-950">#stronywww</div>
+        <div className="relative left-72 text-amber-400 shadow-stone-950">
+          #aplikacjeweb
+        </div>
       </div>
       <Image
-        src="/hero.webp"
-        className="z-10 relative mx-auto"
+        src="/hero-simple.webp"
+        className="z-10 relative mx-auto opacity-50"
         width={926}
         height={500}
         alt="Francky"
