df9d7269eeef4ca02d822b43258f93c88b7894ab
Francky <contact@francky.pl>
2024-01-20 11:04:39 +0100

    update meta description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const exo = Exo({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Francky Iside - Web development",
   description:
-    "Nazywam się Francky Iside. Jestem web developerem z Gdańska. Specjalizuję się w projektowaniu, oraz w tworzeniu stron internetowych i aplikacji web. Wykorzystuję technologie TypeScript, Php, React, Wordpress i Next.js.",
+    "Nazywam się Francky Iside. Jestem frontend developerem specjalizującym się w projektowaniu responsywnych, wydajnych stron internetowych, aplikacji SPA i sklepów eCommerce. Wykorzystuję technologie TypeScript, React, Next.js, Php, oraz WordPress.",
 };
 
 export default function RootLayout({
