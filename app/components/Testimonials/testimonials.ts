8074f634c6464b68d718a0b886743061c48de46b
Francky <contact@francky.pl>
2024-01-21 20:03:55 +0100

    add testimonials (in progress)

---
+++ app/components/Testimonials/testimonials.ts
@@ -0,0 +1,67 @@
+export const testimonials = [
+  {
+    author: "Maciej Brzezicki",
+    company: "Chief Operating Officer",
+    desc: '"I have had the pleasure of working with Francky on many important projects. Francky is very creative and has shown the important ability to use his skills to help develop Trec Nutrition products and make them attractive. He is very focused, conscientious, understands the importance of deadlines and is willing to work. I rate him as a high-class specialist and recommend him to work in the best teams."',
+  },
+  {
+    author: "Dominika Paneczko",
+    company: "Właścicielka Folwark Dajak",
+    desc: "Pan Francky jest świetnym profesjonalistą! Współpracujemy już od pewnego czasu i jestem bardzo zadowolona z jego usług. Każde zadanie wykonuje bardzo rzetelnie oraz ceny są bardzo konkurencyjne! Serdecznie polecam!",
+  },
+  {
+    author: "Michał Łuński",
+    company: "Export Manager - Trec Nutrition",
+    desc: "Pan Francky jest świetnym profesjonalistą! Współpracujemy już od pewnego czasu i jestem bardzo zadowolona z jego usług. Każde zadanie wykonuje bardzo rzetelnie oraz ceny są bardzo konkurencyjne! Serdecznie polecam!",
+  },
+  {
+    author: "Wiesław M.",
+    company: "Właściciel strony dlamaluszka.waw.pl",
+    desc: '"Bardzo dobry kontakt, szybko skutecznie i profesjonalnie. Atrakcyjna cena, super doradztwo, polecam współpracę z Inside. W razie problemów ze stroną na pewno wrócę do Inside."',
+  },
+  {
+    author: "Rebecca Jimenez",
+    company: "SEO Content Coordinator at Catena Media",
+    desc: '"Franky is one of the best colleagues I have been working with. For his work at the company I can tell he was delivering great quality and working very fast. He is very passionate on what he is doing and a great team player. Any person will be lucky to have him, I super recommend him!"',
+  },
+  {
+    author: "Kasia",
+    company: "Właścicielka szkoły Fortissimus",
+    desc: '"Profesjonalna obsługa, szybka reakcja na każdą sugestię klienta. Zarówno cała strona internetowa, jak i wszelkie prace graficzne zrobione zgodnie z moimi oczekiwaniami. Serdecznie polecam każdemu, kto ceni sobie czas i profesjonalnie wykonane usługi zgodne z zaleceniami i oczekiwaniami."',
+  },
+  {
+    author: "Marta",
+    company: "Właścicielka Sex Shop Pleasure",
+    desc: "Bardzo polecam, profesjonaliści!",
+  },
+  {
+    author: "Radosław Swiecki",
+    company: "Grafik komputerowy",
+    desc: '"I\'ve been working with Francky for a year and it was very fruitful and smooth cooperation. Francky is a fast, hard-working employee, always prompt to tackle a task, fully engaged in a project and passionate about his work. He easily finds his place in multi-cultural environment, is a kind-hearthed person and maintains great relations with other team members."',
+  },
+  {
+    author: "Michał D.",
+    company: "Właściciel firmy DSGN",
+    desc: '"Bardzo sypmtaczna osoba, kontakt wzorcowy, wszystko zrealizowane zgodnie z potrzebami! POLECAM -www.daddys.pl"',
+  },
+  {
+    author: "Marcin Skoniecki",
+    company: "Grafik komputerowy",
+    desc: "\"I worked with Francky during last two years at Trec Nutrition. Above all, I can confirm Francky is a very skilled Specialist, with many year' experience, and his work manifests pure passion. What was a privilege for me, he's always ready to share his knowledge, his know-how with me. This enriched my experience a lot, and my skills repertoire. Thank you Francky!\"",
+  },
+  {
+    author: "Łukasz Grucza",
+    company: "Księgowy",
+    desc: '"Polecam współpracę z Arte Web wszystkim, którzy potrzebują usług grafiki komputerowej. Godni zaufania, profesjonalni, pomocni zarówno podczas realizacji jak i już po zakończonych projektach, szybki kontakt, konkretni, solidni, rzetelni i terminowi."',
+  },
+  {
+    author: "Mateusz Deja",
+    company: "Sauber Serwis",
+    desc: '"Polecam współpracę z Franckym, profesjonalne podejscie oraz cierpliwość w spełnianiu życzeń klienta. Strona zrobiona, teraz zaczynamy współpracę przy pozycjonowaniu."',
+  },
+  {
+    author: "Daniel B.",
+    company: "Specjalista ds. Marketingu",
+    desc: '"Polecam, szybka sprawna realizacja, zlecenie wykonane bez problemów!"',
+  },
+];
