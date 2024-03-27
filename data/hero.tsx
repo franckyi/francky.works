566b7bc7a1a23598335289075e4dc414a5c3bace
Francky Iside <126207190+franckyi@users.noreply.github.com>
2024-03-27 21:35:26 +0100

    Update bio

--- data/hero.tsx
+++ data/hero.tsx
@@ -3,26 +3,7 @@ export const heroSubheading = "JavaScript developer with React/Next.js";
 export const getHeroText = () => {
   return (
     <p className="max-w-screen-xl text-left text-lg dark:text-slate-500">
-      I&apos;m a{" "}
-      <strong>
-        JavaScript developer with a proactive, problem solving attitude
-      </strong>
-      . Since 2016 working with clients building company and e-commerce
-      websites. In 2021 covered a role as Junior Frontend Developer at
-      iGamingNuts for 2+ years.{" "}
-      <strong>
-        Hands-on experience with React, Angular, jQuery, popular frontend
-        libraries and UI/UX
-      </strong>
-      . Currently creating custom software and e-commerce solutions. I bring a
-      <strong>
-        {" "}
-        creative mindset with an eye on aesthetics, SOLID principles, clean
-        code, RWD and Core Web Vitals
-      </strong>
-      . Altough I really enjoy Angular, React best fits most of my current
-      clients needs. I&apos;d like to contribute my skills to ambitious,
-      interesting projects. Join me on LinkedIn and feel free to contact me.
+      I&apos;m a JavaScript developer with love for creative design. Based in Trójmiasto, Poland. Crafting tailored web solutions & e-commerce websites. A solid design background, strong creativity and great communication skills. I specialise in JavaScript and TypeScript. Mainly using React/Next.js, some projects done with Angular. Full Stack oriented with Node.js. Also familiar with jQuery, PHP, Strapi CMS, WordPress and WooCommerce APIs, PrestaShop. For design I use Figma, Affinity Photo & Designer, Inkscape, CSS/SASS and Tailwind.
     </p>
   );
 };
