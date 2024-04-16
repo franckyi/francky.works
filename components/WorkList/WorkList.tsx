a8ba7aea20add3fdec6faefc3a0118bd436107cc
Francky <contact@francky.pl>
2024-04-16 19:04:22 +0200

    fix work structure

--- components/WorkList/WorkList.tsx
+++ components/WorkList/WorkList.tsx
@@ -2,20 +2,22 @@ import { Work } from "@/types/Work";
 import WorkItem from "./WorkItem/WorkItem";
 import getWorks from "@/lib/getWorks";
 
-const sectionClasses = "lg:mt-32 max-w-screen-xl lg:mx-auto";
+const sectionClasses = "max-w-screen-xl lg:mx-auto";
 const headingClasses =
-  "my-8 text-3xl lg:text-5xl font-bold dark:text-emerald-400 text-center font-secondary";
+  "lg:mt-32 my-8 text-3xl lg:text-5xl font-bold dark:text-emerald-400 text-center font-secondary";
 
 export default async function WorkList() {
   const works = await getWorks();
 
   return (
-    <section className={sectionClasses}>
+    <>
       <h2 className={headingClasses}>Recent Works</h2>
-      {works &&
-        works.map((work: Work) => {
-          return <WorkItem key={work.id} work={work} />;
-        })}
-    </section>
+      <section className={sectionClasses}>
+        {works &&
+          works.map((work: Work) => {
+            return <WorkItem key={work.id} work={work} />;
+          })}
+      </section>
+    </>
   );
 }
