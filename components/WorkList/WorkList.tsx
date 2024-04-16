e53ce0032a5e61c7e975a7d0db4ac70d5fe6c784
Francky <contact@francky.pl>
2024-04-16 18:36:50 +0200

    more spacing after hero

--- components/WorkList/WorkList.tsx
+++ components/WorkList/WorkList.tsx
@@ -2,7 +2,7 @@ import { Work } from "@/types/Work";
 import WorkItem from "./WorkItem/WorkItem";
 import getWorks from "@/lib/getWorks";
 
-const sectionClasses = "max-w-screen-xl lg:mx-auto";
+const sectionClasses = "lg:mt-32 max-w-screen-xl lg:mx-auto";
 const headingClasses =
   "my-8 text-3xl lg:text-5xl font-bold dark:text-emerald-400 text-center font-secondary";
 
