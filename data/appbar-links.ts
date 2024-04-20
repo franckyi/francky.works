82f4fbd58825121580c3fb481da493ce58fc7dd7
Francky <contact@francky.pl>
2024-04-20 18:02:34 +0200

    update links

--- data/appbar-links.ts
+++ data/appbar-links.ts
@@ -5,15 +5,15 @@ interface Link {
 }
 
 export const menuItems: Link[] = [
-  { name: "GitHub", url: "https://github.com/franckyi", target: "_blank" },
+  { name: "GitHub", url: "https://github.com/franckyrj", target: "_blank" },
   {
     name: "LinkedIn",
-    url: "https://www.linkedin.com/in/franckyiside",
+    url: "https://www.linkedin.com/in/franckyrj",
     target: "_blank",
   },
   {
     name: "v.2022",
-    url: "https://franckyi.github.io/my-portfolio-website-v2022/",
+    url: "https://franckyrj.github.io/my-portfolio-website-v2022/",
     target: "_blank",
   },
 ];
