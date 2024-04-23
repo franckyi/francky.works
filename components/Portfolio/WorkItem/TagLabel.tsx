1b97311b2660a12c1efa26921adf00f71474aab4
Francky <contact@francky.pl>
2024-04-23 14:43:07 +0200

    fix type

--- components/Portfolio/WorkItem/TagLabel.tsx
+++ components/Portfolio/WorkItem/TagLabel.tsx
@@ -7,7 +7,7 @@ const classes =
   "my-1 inline-block mr-2 lg:mr-4 px-4 pt-1 pb-2 text-xs lg:text-sm capitalize bg-labelLight dark:bg-labelDark";
 
 export default function TagLabel({ tagId }: { tagId: number }) {
-  const [tag, setTag] = useState<any>(null);
+  const [tag, setTag] = useState<Tag | null>(null);
 
   useEffect(() => {
     getTag(tagId).then((data) => {
