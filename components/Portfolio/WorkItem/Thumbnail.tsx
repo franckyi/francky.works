90cead1e231d1c5ea9434dee460abfe1e0e6135b
Francky <contact@francky.pl>
2024-04-23 20:07:31 +0200

    fix thumbnail

--- components/Portfolio/WorkItem/Thumbnail.tsx
+++ components/Portfolio/WorkItem/Thumbnail.tsx
@@ -1,13 +1,13 @@
 import getMedia from "@/lib/getMedia";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import Skeleton from "@mui/material/Skeleton";
+// import Skeleton from "@mui/material/Skeleton";
 
-function ThumbnailSkeleton() {
-  return (
-    <Skeleton variant="rectangular" width={645} height={365} animation="wave" />
-  );
-}
+// function ThumbnailSkeleton() {
+//   return (
+//     <Skeleton variant="rectangular" width={645} height={365} animation="wave" />
+//   );
+// }
 
 const baseClasses =
   "rounded-md lg:rounded-2xl shadow-xl shadow-dark/30 dark:shadow-primary/25";
@@ -39,16 +39,14 @@ export default function Thumbnail({ href, alt, view, hidden }: ThumbnailProps) {
     });
   }, [href]);
 
-  return thumbnail ? (
+  return (
     <Image
-      src={thumbnail.media_details.sizes.medium_large.source_url}
+      src={thumbnail?.media_details?.sizes?.medium_large?.source_url}
       alt={alt}
       className={`${baseClasses} ${getMargin()} ${getOpacity()}`}
       width={645}
       height={365}
       draggable="false"
     />
-  ) : (
-    <ThumbnailSkeleton />
   );
 }
