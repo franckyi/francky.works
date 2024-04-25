ff0c390acd756fef05fb70bc4051b8280cac1943
Francky <contact@francky.pl>
2024-04-25 17:01:40 +0200

    remove unused import

--- components/Portfolio/CategoryFilter/CategoryFilter.tsx
+++ components/Portfolio/CategoryFilter/CategoryFilter.tsx
@@ -2,7 +2,6 @@
 import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Works from "../Works";
 import { Work } from "@/types/Work";
